8c3a3d4cb1d51eebfa2523d2ff6eb8c8c5dcc907
Chebupelinka <spb@dyanovsky.ru>
2024-07-04 12:35:29 +0300

    New functions: now, if you got 'Alarm message' you would also got id of person, who sent it!

--- main_bot.py
+++ main_bot.py
@@ -1,27 +1,38 @@
 import telebot
+import json
 
 # Ваш токен бота
 TOKEN = '6794810148:AAFQileW8K-qLqPeprh4bn7H1uKImvWq74Y'
 
+# Загружаем данные из файла people.json
+with open('people.json', 'r', encoding='utf-8') as file:
+    people_data = json.load(file)
+    people_list = people_data['users']
+
 # Создаем объект бота
 bot = telebot.TeleBot(TOKEN)
 
-# Заполняем список людей (можно загрузить из файла или базы данных)
-people_list = ['Иван', 'Мария', 'Петр']
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     for person in people_list:
-        markup.add(telebot.types.KeyboardButton(person))
+        markup.add(telebot.types.KeyboardButton(person['name']))
     bot.send_message(message.chat.id, "Выберите человека:", reply_markup=markup)
 
+
 # Обработчик выбора пользователя
-@bot.message_handler(func=lambda message: message.text in people_list)
+@bot.message_handler(func=lambda message: message.text in [person['name'] for person in people_list])
 def send_notification(message):
-    chosen_person = message.text
-    bot.send_message(message.chat.id, f"Отправлено уведомление '{chosen_person}': Срочно!")
+    chosen_person_name = message.text
+    chosen_person = next(person for person in people_list if person['name'] == chosen_person_name)
+    if chosen_person['urgent']:
+        bot.send_message(message.chat.id, f"Отправлено уведомление '{chosen_person_name}': Срочно!")
+        bot.send_message(chosen_person['id'], f"Alarm! From id:{message.from_user.id}")
+    else:
+        bot.send_message(message.chat.id, f"{chosen_person_name} не в списке срочно.")
+
 
 # Запускаем бота
 bot.polling()
