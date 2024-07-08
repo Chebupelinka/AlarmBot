45aaa1db7abc13fcf6db38e17d47c8f05b93e9ec
Chebupelinka <spb@dyanovsky.ru>
2024-07-08 10:55:52 +0300

    Commit after pull

--- main_bot.py
+++ main_bot.py
@@ -5,9 +5,21 @@ import json
 TOKEN = '6794810148:AAFQileW8K-qLqPeprh4bn7H1uKImvWq74Y'
 
 # Загружаем данные из файла people.json
-with open('people.json', 'r', encoding='utf-8') as file:
+with open('people.json', 'r+', encoding='utf-8') as file:
     people_data = json.load(file)
     people_list = people_data['users']
+
+
+def check_is_user_was_there(user_id):
+    for person in people_list:
+        if user_id == person['id']:
+            return True
+    return False
+
+def update_json(new_data):
+    people_data['users'].append(new_data)
+    file.seek(0)
+    json.dump(people_data, file)
 
 # Создаем объект бота
 bot = telebot.TeleBot(TOKEN)
@@ -16,11 +28,39 @@ bot = telebot.TeleBot(TOKEN)
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
+    if not check_is_user_was_there(message.from_user.id):
+
+        name = ""
+        group = 0
+
+        bot.send_message(message.chat.id, "Приветсвую тебя, давай знакомиться! Меня зовут FKIT_ALARM bot. А тебя? "
+                                               "\n\n\nP.S. Напиши своё настоящее ФИО, чтобы тебя было проще найти!")
+        name = message.text
+
+        bot.send_message(message.chat.id, f"Приятно познаомиться, {name}!")
+        bot.send_message(message.chat.id, "А из какой ты группы?")
+
+        while True:
+            try:
+                group = int(get_text(message))
+                break
+            except ValueError:
+                bot.send_message(message.chat.id, "Упс, похоже кто-то ввёл неправильно группу, попробуй ещё раз))))"
+                                                       "\n Число должно быть формата int!")
+        new_person = {"id": message.from_user.id,
+            "name": name,
+            "group": group,
+            "urgent": False,
+            "admin": False}
+        update_json(new_person)
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     for person in people_list:
         markup.add(telebot.types.KeyboardButton(person['name']))
     bot.send_message(message.chat.id, "Выберите человека:", reply_markup=markup)
 
+@bot.message_handler(func=lambda message: True)
+def get_text(message):
+    return message.text
 
 # Обработчик выбора пользователя
 @bot.message_handler(func=lambda message: message.text in [person['name'] for person in people_list])
